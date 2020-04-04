29c4293c17155649782a30cf0660dc026f042144
szykol <szymonkolton@gmail.com>
2020-04-04 18:54:13 +0200

    Add DB unittests with mocks

--- test/unittests.py
+++ test/unittests.py
@@ -1,11 +1,13 @@
-from unittest import TestCase
-
-from sanic import response
+from unittest import TestCase, mock
+import asynctest
+from sanic import response, request
 from ddt import ddt, data, unpack
 import os, sys
+import pytest
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.response import Responses, create_response
+from src.db import DB, asyncpg
 
 class UnitTestsBase(TestCase):
     def setUp(self):
@@ -25,6 +27,31 @@ class ResponseUnitTest(UnitTestsBase):
     def test_create_response_correct_status(self, input_response):
         resp = create_response(input_response)
         self.assertEqual(resp.status, input_response.value["status"])
+
+class AsyncDBUnitTest(asynctest.TestCase):
+    @mock.patch('src.db.asyncpg.create_pool', new_callable=mock.AsyncMock)
+    async def test_create_pool(self, mockpg):
+        DSN = 'unit://unit:unit@unit:5432/_unit'
+        await DB.init(dsn=DSN)
+        mockpg.assert_called_with(dsn=DSN)
+
+    @mock.patch('src.db.asyncpg.create_pool', new_callable=mock.AsyncMock)
+    async def test_get_pool(self, mockpg):
+        DSN = 'unit://unit:unit@unit:5432/_unit'
+        mocked_pool = mock.Mock(asyncpg.pool.Pool)
+        mockpg.return_value = mocked_pool
+        await DB.init(dsn=DSN)
+        pool = DB.get_pool()
+        self.assertIsNotNone(pool)
+        self.assertIsInstance(pool, asyncpg.pool.Pool)
+
+    async def test_get_pool_without_init(self):
+        # simulate DB before init
+        DB._pool = None
+        pool = DB.get_pool()
+        self.assertIsNone(pool)
+
+
 
 if __name__ == "__main__":
     unittest.main()
