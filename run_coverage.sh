fb90d31cf7d1e7e563ce1ca0f910ed859f1902c7
szykol <szymonkolton@gmail.com>
2020-04-10 22:41:30 +0200

    Fix coverage

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,7 +1,7 @@
 python -m coverage erase
-python -m coverage run api.py & > /dev/null 2>&1 
+python -m coverage run run.py & > /dev/null 2>&1 
 SERVER_PID=$!
-API_TEST="coverage" pytest test/endtoend/* > /dev/null 2>&1
+API_TEST="coverage" python -m pytest test/endtoend/* > /dev/null 2>&1
 kill $SERVER_PID
 echo "Results for end to end tests"
 python -m coverage report
@@ -11,9 +11,9 @@ echo "Results for unit tests"
 python -m coverage report
 
 
-python -m coverage run api.py & > /dev/null 2>&1 
+python -m coverage run run.py & > /dev/null 2>&1 
 SERVER_PID=$!
-API_TEST="coverage" pytest test/endtoend/* > /dev/null 2>&1
+API_TEST="coverage" python -m pytest test/endtoend/* > /dev/null 2>&1
 kill $SERVER_PID
 python -m coverage run -m -a pytest test/* > /dev/null 2>&1
 echo ""
